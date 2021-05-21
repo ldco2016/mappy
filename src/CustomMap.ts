8870a0e39c6242d33eb092c5ab884fb6e1945c11
ldco2016 <renaissance.scholar2012@gmail.com>
2021-05-20 19:11:27 -0500

    addCircleMarker event listener issues

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -12,15 +12,17 @@ export interface Mappable {
 }
 
 export class CustomMap {
-  private googleMap: google.maps.Map;
+  public googleMap: google.maps.Map;
 
+  // here is where I am initializing the map to be upon boot up
   constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
-      zoom: 13,
+      zoom: 6,
       center: {
-        lat: 37.804363,
-        lng: -122.271111,
+        lat: 36.778259,
+        lng: -119.417931,
       },
+      disableDefaultUI: true,
       styles: [
         {
           featureType: "water",
@@ -200,28 +202,389 @@ export class CustomMap {
     });
   }
 
-  // addMarker(mappable: Mappable): void {
-  //   const marker = new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: mappable.Latitude,
-  //       lng: mappable.Longitude,
-  //     },
-  //   });
+  // this method gets applied later inside `index.ts` when
+  // user clicks on 'Oakland, CA' from dropdown
+  addOakland(): void {
+    const oak = (this.googleMap = new google.maps.Map(
+      document.getElementById("map"),
+      {
+        zoom: 12,
+        center: {
+          lat: 37.804363,
+          lng: -122.271111,
+        },
+        disableDefaultUI: true,
+        styles: [
+          {
+            featureType: "water",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#e9e9e9",
+              },
+              {
+                lightness: 17,
+              },
+            ],
+          },
+          {
+            featureType: "landscape",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#f5f5f5",
+              },
+              {
+                lightness: 20,
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#ffffff",
+              },
+              {
+                lightness: 17,
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                color: "#ffffff",
+              },
+              {
+                lightness: 29,
+              },
+              {
+                weight: 0.2,
+              },
+            ],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#ffffff",
+              },
+              {
+                lightness: 18,
+              },
+            ],
+          },
+          {
+            featureType: "road.local",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#ffffff",
+              },
+              {
+                lightness: 16,
+              },
+            ],
+          },
+          {
+            featureType: "poi",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#f5f5f5",
+              },
+              {
+                lightness: 21,
+              },
+            ],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#dedede",
+              },
+              {
+                lightness: 21,
+              },
+            ],
+          },
+          {
+            elementType: "labels.text.stroke",
+            stylers: [
+              {
+                visibility: "on",
+              },
+              {
+                color: "#ffffff",
+              },
+              {
+                lightness: 16,
+              },
+            ],
+          },
+          {
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                saturation: 36,
+              },
+              {
+                color: "#333333",
+              },
+              {
+                lightness: 40,
+              },
+            ],
+          },
+          {
+            elementType: "labels.icon",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "transit",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#f2f2f2",
+              },
+              {
+                lightness: 19,
+              },
+            ],
+          },
+          {
+            featureType: "administrative",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#fefefe",
+              },
+              {
+                lightness: 20,
+              },
+            ],
+          },
+          {
+            featureType: "administrative",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                color: "#fefefe",
+              },
+              {
+                lightness: 17,
+              },
+              {
+                weight: 1.2,
+              },
+            ],
+          },
+        ],
+      }
+    ));
+  }
 
-  //   marker.addListener("click", () => {
-  //     const infoWindow = new google.maps.InfoWindow({
-  //       content: mappable.markerContent(),
-  //     });
+  // this method gets applied later inside `index.ts` when
+  // user clicks on 'San Francisco, CA' from dropdown
+  addSanFran(): void {
+    this.googleMap = new google.maps.Map(document.getElementById("map"), {
+      zoom: 12,
+      center: {
+        lat: 37.733795,
+        lng: -122.446747,
+      },
+      disableDefaultUI: true,
+      styles: [
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#e9e9e9",
+            },
+            {
+              lightness: 17,
+            },
+          ],
+        },
+        {
+          featureType: "landscape",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#f5f5f5",
+            },
+            {
+              lightness: 20,
+            },
+          ],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#ffffff",
+            },
+            {
+              lightness: 17,
+            },
+          ],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [
+            {
+              color: "#ffffff",
+            },
+            {
+              lightness: 29,
+            },
+            {
+              weight: 0.2,
+            },
+          ],
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#ffffff",
+            },
+            {
+              lightness: 18,
+            },
+          ],
+        },
+        {
+          featureType: "road.local",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#ffffff",
+            },
+            {
+              lightness: 16,
+            },
+          ],
+        },
+        {
+          featureType: "poi",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#f5f5f5",
+            },
+            {
+              lightness: 21,
+            },
+          ],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#dedede",
+            },
+            {
+              lightness: 21,
+            },
+          ],
+        },
+        {
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              visibility: "on",
+            },
+            {
+              color: "#ffffff",
+            },
+            {
+              lightness: 16,
+            },
+          ],
+        },
+        {
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              saturation: 36,
+            },
+            {
+              color: "#333333",
+            },
+            {
+              lightness: 40,
+            },
+          ],
+        },
+        {
+          elementType: "labels.icon",
+          stylers: [
+            {
+              visibility: "off",
+            },
+          ],
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#f2f2f2",
+            },
+            {
+              lightness: 19,
+            },
+          ],
+        },
+        {
+          featureType: "administrative",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#fefefe",
+            },
+            {
+              lightness: 20,
+            },
+          ],
+        },
+        {
+          featureType: "administrative",
+          elementType: "geometry.stroke",
+          stylers: [
+            {
+              color: "#fefefe",
+            },
+            {
+              lightness: 17,
+            },
+            {
+              weight: 1.2,
+            },
+          ],
+        },
+      ],
+    });
+  }
 
-  //     infoWindow.open(this.googleMap, marker);
-  //   });
-  // }
-
-  // for (const city in Data) {
-
-  // }
-
+  // makes the red dots happen including the listener
+  // for clicking the dots and seeing their label
   addCircleMarker(mappable: Mappable): void {
     const circleMarker = new google.maps.Circle({
       strokeColor: "#FF0000",
@@ -247,13 +610,5 @@ export class CustomMap {
 
       infoWindow.open(this.googleMap);
     });
-
-    // circleMarker.addListener("click", () => {
-    //   const infoWindow = new google.maps.InfoWindow({
-    //     content: mappable.markerContent(),
-    //   });
-
-    //   infoWindow.open(this.googleMap, circleMarker);
-    // });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,60 @@ for (const rawRow of rawData.split(ROW_DELIMITER)) {
 const customMap = new CustomMap("map");
 
 data.forEach((label) => customMap.addCircleMarker(label));
+
+// The Dropdown for Cities
+const root = document.querySelector(".cities");
+root.innerHTML = `
+<div class="dropdown">
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+      <span>Choose a city</span>
+      <span class="icon is-small">
+        <!--fontawesome required for the icon-->
+        <i class="fa fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu" role="menu">
+    <div class="dropdown-content">
+      <a href='#' data-value="oak" class='oak dropdown-item'>
+        Oakland, CA
+      </a>
+      <a href='#' data-value="san" class='san dropdown-item'>
+        San Francisco, CA
+      </a>
+    </div>
+  </div>
+</div>
+`;
+
+const dropdown = document.querySelector(".dropdown");
+
+document.querySelector(".dropdown-trigger").addEventListener("click", (e) => {
+  dropdown.classList.toggle("is-active");
+});
+
+document.addEventListener(
+  "click",
+  (event) => {
+    event.preventDefault();
+    if (event !== null && event.target instanceof HTMLElement) {
+      const element = event.target;
+      if (element.matches(".dropdown-item.oak")) {
+        console.log("Oakland was clicked");
+
+        customMap.addOakland();
+        data.forEach((label) => customMap.addCircleMarker(label));
+        console.log("inside the if conditional", data);
+        dropdown.classList.toggle("active");
+      }
+      if (element.matches(".dropdown-item.san")) {
+        console.log("San Francisco was clicked");
+        customMap.addSanFran();
+        data.forEach((label) => customMap.addCircleMarker(label));
+        dropdown.classList.toggle("active");
+      }
+    }
+  },
+  false
+);
